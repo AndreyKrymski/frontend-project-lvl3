ba3e55ab35c595cd10c0b96314c77027c7ba54f9
Andrey Krymski <6714223@mail.ru>
2022-08-21 23:38:32 +0300

    step2

--- src/init.js
+++ src/init.js
@@ -1,9 +1,59 @@
 // @ts-check
+import * as yup from 'yup';
+import onChange from 'on-change';
 
-import Example from './Example.js';
-
+const elements = {
+  posts: document.querySelector('.posts'),
+  feeds: document.querySelector('.feeds'),
+  textBody: document.querySelector('.text-body'),
+  textFeedback: document.querySelector('.feedback'),
+  urlInput: document.getElementById('url-input'),
+};
+const render = (state) => {
+  console.log(state.erorr);
+  if (state.isValid) {
+    elements.textFeedback.textContent = 'RSS успешно загружен';
+    elements.textFeedback.classList.add('text-success');
+    elements.textFeedback.classList.remove('text-danger');
+    elements.urlInput.classList.remove('is-invalid');
+    elements.textBody.reset();
+    elements.urlInput.focus();
+  } else {
+    elements.textFeedback.textContent = state.erorr.join();
+    elements.urlInput.classList.remove('text-success');
+    elements.urlInput.classList.add('is-invalid');
+    elements.textFeedback.classList.remove('text-success');
+    elements.textFeedback.classList.add('text-danger');
+  }
+};
 export default () => {
-  const element = document.getElementById('point');
-  const obj = new Example(element);
-  obj.init();
+  const state = {
+    isValid: null,
+    url: [],
+    erorr: [],
+  };
+  const watcheState = onChange(state, () => {
+    render(state);
+  });
+
+  const validateUrl = (link) => {
+    const schema = yup.string().url('Ссылка должна быть валидным URL').notOneOf(state.url, 'RSS уже существует');
+    return schema.validate(link)
+      .then(() => {
+        state.url.push(link);
+        state.erorr = [];
+        watcheState.isValid = true;
+      })
+      .catch((e) => {
+        state.erorr.push(e.message);
+        watcheState.isValid = false;
+      });
+  };
+
+  elements.textBody.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const form = new FormData(e.target);
+    const value = form.get('url');
+    validateUrl(value);
+  });
 };
