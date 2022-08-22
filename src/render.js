742ae76925e326bfbff880ae8fce250d7c07d72c
Andrey Krymski <6714223@mail.ru>
2022-08-22 19:47:20 +0300

    not add setlocate

--- src/render.js
+++ src/render.js
@@ -5,17 +5,14 @@ export default function render(state, i18nextInstance) {
 
   if (state.statusValidation === 'valid') {
     elements.textFeedback.textContent = i18nextInstance.t('status.valid');
-    elements.textFeedback.classList.add('text-success');
-    elements.textFeedback.classList.remove('text-danger');
+    elements.textFeedback.classList.replace('text-danger','text-success');
     elements.urlInput.classList.remove('is-invalid');
     elements.textBody.reset();
     elements.urlInput.focus();
   }
-  if (state.statusValidation === 'invalid') {
+  else if (state.statusValidation === 'invalid') {
     elements.textFeedback.textContent = state.erorr;
-    elements.urlInput.classList.remove('text-success');
     elements.urlInput.classList.add('is-invalid');
-    elements.textFeedback.classList.remove('text-success');
-    elements.textFeedback.classList.add('text-danger');
+    elements.textFeedback.classList.replace('text-success', 'text-danger');
   }
 }
